29ce83697c5afc26cefa346c1ce1dee128d14959
Juri <jasonluxie@gmail.com>
2021-10-28 18:18:38 -0500

    added some routes

--- models/index.js
+++ models/index.js
@@ -1,20 +1,35 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+    foreignKey: "category_id",
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+    foreignKey: "category_id",
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(ProductTag, {
+    through: {
+        model: "product_tag",
+        unique: false,
+    },
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, {
+    through: {
+        model: "product_tag",
+        unique: false,
+    },
+});
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+    Product,
+    Category,
+    Tag,
+    ProductTag,
 };
