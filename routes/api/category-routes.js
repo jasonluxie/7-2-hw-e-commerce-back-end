1a08d8f2b64d417b96fd288207c32e6472eba6ac
Juri <jasonluxie@gmail.com>
2021-10-28 18:45:03 -0500

    categories updated

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,25 +20,55 @@ router.get("/:id", async (req, res) => {
     // find one category by its `id` value
     // be sure to include its associated Products
     try {
-      const categoryData = await Category.findByPk(req.params.id, {
-        include: [{model: Product}]
-      })
-      if (!categoryData) {
-        res.status(404).json({message : `Sorry, that doesn't seem to exist!`})
-      }
+        const categoryData = await Category.findByPk(req.params.id, {
+            include: [{ model: Product }],
+        });
+        if (!categoryData) {
+            res.status(404).json({
+                message: `Sorry, that doesn't seem to exist!`,
+            });
+        } 
+        res.status(200).json(categoryData)
+    } catch (err) {
+        res.status(500).json(err);
     }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
     // create a new category
+    try {
+        const categoryData = await Category.create(req.body);
+        res.status(200).json(`Category created!`, categoryData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
     // update a category by its `id` value
+    try {
+        const categoryData = await Category.update(
+            { category_name: req.body.category_name },
+            { where: req.params.id }
+        );
+        res.status(200).json(`Category updated!`, categoryData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
     // delete a category by its `id` value
+    try {
+        const categoryData = await Category.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        res.status(200).json(`Category deleted`, categoryData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
